408d1e8d8668336bd7b715043ff3de1f63deee01
stegraphics <stefanoschifanographic@gmail.com>
2025-07-16 22:34:13 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,4 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  }
 });
